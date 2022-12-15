8129d55ca6ff824bf11dbf06ba6be54335d546d3
MisterCruz <pspnando@me.com>
2022-12-15 13:57:16 -0800

    update hero component

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -6,6 +6,9 @@ const Hero = () => {
   return (
     <div className="text-[#1f2937]">
       <div className="mx-auto w-full h-screen justify-center flex mt-[-96px] max-w-[800px] text-center flex-col">
+        <p className="font-bold md:text-4xl sm:text-3xl text-2xl md:py-6">
+          Project management for your needs
+        </p>
         <h1 className="font-bold md:text-7xl sm:text-6xl text-4xl md:py-6">
           Keep track of your tickets and bugs
         </h1>
